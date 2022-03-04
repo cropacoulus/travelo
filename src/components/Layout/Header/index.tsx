3e688b5c9257515b9c8a940dcb722883637710d1
cropacoulus <desmar217@gmail.com>
2022-03-04 10:52:04 +0700

    Finsihed Component Header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,18 +1,33 @@
 import Image from 'next/image'
+import Container from 'components/Container'
+import Button from 'components/Button'
+import Search from './Search'
+import Nav from './Nav'
 import traveloLogo from 'public/assets/travelo-logo-header.png'
 
 const Header = () => {
     return (
         <header className="h-[85px] bg-white shadow-lg p-4">
-            <div className="flex justify-between items-center">
-                <Image
-                    src={traveloLogo}
-                    alt="Travelo Logo"
-                    placeholder="blur"
-                    height="48"
-                    width="182"
-                />
-            </div>
+            <Container>
+                <div className="flex justify-between items-center">
+                    <div className="flex">
+                        <Image
+                            src={traveloLogo}
+                            alt="Travelo Logo"
+                            placeholder="blur"
+                            height="48"
+                            width="182"
+                        />
+                        <Search/>
+                    </div>
+                    <div className="flex gap-5">
+                        <Nav/>
+                        <Button className="w-[190px]">
+                            Masuk
+                        </Button>
+                    </div>
+                </div>
+            </Container>
         </header>
     )
 }
