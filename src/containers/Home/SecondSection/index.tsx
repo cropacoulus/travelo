15515d6f82d384a06b6d46d4a1af5fb827e9428c
cropacoulus <desmar217@gmail.com>
2022-03-05 12:17:56 +0700

    Packages & PackageCard Component

--- src/containers/Home/SecondSection/index.tsx
+++ src/containers/Home/SecondSection/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import Container from 'components/Container'
 import PromoCarousel from './PromoCarousel'
+import Packages from './Packages'
 import styles from './SecondSection.module.css'
 import 'react-multi-carousel/lib/styles.css'
 const SecondSection = () => {
@@ -8,6 +8,7 @@ const SecondSection = () => {
         <section className={styles.section}>
             <Container>
                 <PromoCarousel/>
+                <Packages/>
             </Container>
         </section>
     )
