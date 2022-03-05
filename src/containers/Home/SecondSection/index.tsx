6c482db8dee8056e3d021487a2f57ac372bc9e62
cropacoulus <desmar217@gmail.com>
2022-03-05 12:37:21 +0700

    Categories & CategoryCard Component

--- src/containers/Home/SecondSection/index.tsx
+++ src/containers/Home/SecondSection/index.tsx
@@ -1,14 +1,17 @@
 import Container from 'components/Container'
 import PromoCarousel from './PromoCarousel'
 import Packages from './Packages'
+import Categories from './Categories'
 import styles from './SecondSection.module.css'
 import 'react-multi-carousel/lib/styles.css'
+
 const SecondSection = () => {
     return (
         <section className={styles.section}>
             <Container>
                 <PromoCarousel/>
                 <Packages/>
+                <Categories/>
             </Container>
         </section>
     )
