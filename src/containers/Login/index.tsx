61afdc8d7e5951998d55c447d9af2630fe309ee6
cropacoulus <desmar217@gmail.com>
2022-03-07 19:41:59 +0700

    Form Wrapper, Input Component, Implement Input Component

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,3 +1,5 @@
+import Button from 'components/Button'
+import Input from 'components/Input'
 import Layout from 'components/Layout'
 import styles from './Login.module.css'
 
@@ -5,7 +7,47 @@ const Login = () => {
     return(
             <Layout noFooter>
                 <section className={styles.section}>
-                        
+                    <div className={styles.formWrapper}>
+                        <p className="text-heading-3 font-label font-extrabold">Masuk</p>
+
+                        <div className="flex mt-3">
+                            <p className="text-heading-5 text-gray-50 font-semibold">
+                                Belum punya akun?    
+                            </p>    
+                            <a 
+                                href="#"
+                                className="text-heading-5 text-blue-100 font-semibold ml-2 hover:underline"
+                            >
+                                Daftar Di sini
+                            </a>
+                        </div>
+
+                        <form className="flex flex-col mt-8">
+                            <Input
+                                label="Email"
+                                type="email"
+                                name="email"
+                                placeholder="Masukkan alamat email"
+                                className="mb-6"
+                            />
+                            <Input
+                                label="Password"
+                                type="password"
+                                name="password"
+                                placeholder="Masukkan kata sandi"
+                            />
+
+                            <a 
+                                href="#"
+                                className="text-heading-5 text-blue-100 font-semibold mt-8 mb-4 hover:underline"
+                            >
+                                Lupa kata sandi?
+                            </a>
+
+                            <Button>Masuk</Button>    
+                        </form>
+
+                    </div>
                 </section>
             
             </Layout>
