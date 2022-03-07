64a1392fc0ee27eff4716c037fd696f2d54f113b
cropacoulus <desmar217@gmail.com>
2022-03-07 18:54:33 +0700

    Create Login Page Section

---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,15 @@
+import Layout from 'components/Layout'
+import styles from './Login.module.css'
+
+const Login = () => {
+    return(
+            <Layout noFooter>
+                <section className={styles.section}>
+                        
+                </section>
+            
+            </Layout>
+    )
+}
+
+export default Login
